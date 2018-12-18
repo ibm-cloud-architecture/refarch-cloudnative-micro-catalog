79b0a6705b10bcec1538d277d23c85a7e8b5e258
Fabio Gomez <fabiogomez@us.ibm.com>
2018-12-18 10:19:10 -0600

    - First attempt

---
+++ jenkins/Jenkinsfile-build.groovy
@@ -0,0 +1,194 @@
+/*
+    To learn how to use this sample pipeline, follow the guide below and enter the
+    corresponding values for your environment and for this repository:
+    - https://github.com/ibm-cloud-architecture/refarch-cloudnative-devops-kubernetes
+*/
+
+// Environment
+def clusterURL = env.CLUSTER_URL
+def clusterAccountId = env.CLUSTER_ACCOUNT_ID
+def clusterCredentialId = env.CLUSTER_CREDENTIAL_ID ?: "cluster-credentials"
+
+// Pod Template
+def podLabel = "catalog"
+def cloud = env.CLOUD ?: "kubernetes"
+def registryCredsID = env.REGISTRY_CREDENTIALS ?: "registry-credentials-id"
+def serviceAccount = env.SERVICE_ACCOUNT ?: "jenkins"
+
+// Pod Environment Variables
+def namespace = env.NAMESPACE ?: "default"
+def registry = env.REGISTRY ?: "docker.io"
+def imageName = env.IMAGE_NAME ?: "ibmcase/bluecompute-catalog"
+def serviceLabels = env.SERVICE_LABELS ?: "app=catalog,tier=backend" //,version=v1"
+def microServiceName = env.MICROSERVICE_NAME ?: "catalog"
+def servicePort = env.MICROSERVICE_PORT ?: "8081"
+def managementPort = env.MANAGEMENT_PORT ?: "8091"
+
+// External Test Database Parameters
+// For username and passwords, set ES_USER (as string parameter) and ES_PASSWORD (as password parameter)
+//     - These variables get picked up by the Java application automatically
+//     - There were issues with Jenkins credentials plugin interfering with setting up the password directly
+
+def elasticSearchHost = env.ES_HOST
+def elasticSearchPort = env.ES_PORT ?: "3306"
+def elasticSearchDatabase = env.ES_DATABASE ?: "catalogdb"
+def elasticSearchCredsId = env.ES_CREDENTIALS ?: "catalog-es-id"
+
+/*
+  Optional Pod Environment Variables
+ */
+def helmHome = env.HELM_HOME ?: env.JENKINS_HOME + "/.helm"
+
+podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, namespace: namespace, envVars: [
+        envVar(key: 'CLUSTER_URL', value: clusterURL),
+        envVar(key: 'CLUSTER_ACCOUNT_ID', value: clusterAccountId),
+        envVar(key: 'NAMESPACE', value: namespace),
+        envVar(key: 'REGISTRY', value: registry),
+        envVar(key: 'IMAGE_NAME', value: imageName),
+        envVar(key: 'SERVICE_LABELS', value: serviceLabels),
+        envVar(key: 'MICROSERVICE_NAME', value: microServiceName),
+        envVar(key: 'MICROSERVICE_PORT', value: servicePort),
+        envVar(key: 'MANAGEMENT_PORT', value: managementPort),
+        envVar(key: 'ES_HOST', value: elasticSearchHost),
+        envVar(key: 'ES_PORT', value: elasticSearchPort),
+        envVar(key: 'ES_DATABASE', value: elasticSearchDatabase),
+        envVar(key: 'HELM_HOME', value: helmHome)
+    ],
+    volumes: [
+        hostPathVolume(mountPath: '/home/gradle/.gradle', hostPath: '/tmp/jenkins/.gradle'),
+        hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')
+    ],
+    containers: [
+        containerTemplate(name: 'jdk', image: 'ibmcase/openjdk-bash:latest', ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'docker' , image: 'ibmcase/docker-bash:latest', ttyEnabled: true, command: 'cat')
+  ]) {
+
+    node(podLabel) {
+        checkout scm
+
+        // Local
+        container(name:'jdk', shell:'/bin/bash') {
+            stage('Local - Build and Unit Test') {
+                sh """
+                #!/bin/bash
+                ./gradlew build
+                """
+            }
+            stage('Local - Run and Test') {
+                sh """
+                #!/bin/bash
+
+                JAVA_OPTS="-Dspring.datasource.url=jdbc:es://${ES_HOST}:${ES_PORT}/${ES_DATABASE}"
+                JAVA_OPTS="\${JAVA_OPTS} -Dspring.datasource.port=${ES_PORT}"
+                JAVA_OPTS="\${JAVA_OPTS} -Dserver.port=${MICROSERVICE_PORT}"
+
+                java \${JAVA_OPTS} -jar build/libs/micro-catalog-0.0.1.jar &
+
+                PID=`echo \$!`
+
+                # Let the application start
+                bash scripts/health_check.sh "http://127.0.0.1:${MANAGEMENT_PORT}"
+
+                # Run tests
+                set +x
+                bash scripts/api_tests.sh 127.0.0.1 ${MICROSERVICE_PORT}
+                set -x;
+
+                # Kill process
+                kill \${PID}
+                """
+            }
+        }
+
+        // Docker
+        container(name:'docker', shell:'/bin/bash') {
+            stage('Docker - Build Image') {
+                sh """
+                #!/bin/bash
+
+                # Get image
+                if [ "${REGISTRY}" == "docker.io" ]; then
+                    IMAGE=${IMAGE_NAME}:${env.BUILD_NUMBER}
+                else
+                    IMAGE=${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${env.BUILD_NUMBER}
+                fi
+
+                docker build -t \${IMAGE} .
+                """
+            }
+            stage('Docker - Run and Test') {
+                sh """
+                #!/bin/bash
+
+                # Get image
+                if [ "${REGISTRY}" == "docker.io" ]; then
+                    IMAGE=${IMAGE_NAME}:${env.BUILD_NUMBER}
+                else
+                    IMAGE=${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${env.BUILD_NUMBER}
+                fi
+
+                # Kill Container if it already exists
+                docker kill ${MICROSERVICE_NAME} || true
+                docker rm ${MICROSERVICE_NAME} || true
+
+                # Start Container
+                echo "Starting ${MICROSERVICE_NAME} container"
+                set +x
+                docker run --name ${MICROSERVICE_NAME} -d \
+                    -p ${MICROSERVICE_PORT}:${MICROSERVICE_PORT} \
+                    -p ${MANAGEMENT_PORT}:${MANAGEMENT_PORT} \
+                    -e SERVICE_PORT=${MICROSERVICE_PORT} \
+                    -e ES_HOST=${ES_HOST} \
+                    -e ES_PORT=${ES_PORT} \
+                    -e ES_USER=${ES_USER} \
+                    -e ES_PASSWORD=${ES_PASSWORD} \
+                    -e ES_DATABASE=${ES_DATABASE} \${IMAGE}
+                set -x
+
+                # Check that application started successfully
+                docker ps
+
+                # Check the logs
+                docker logs -f ${MICROSERVICE_NAME} &
+                PID=`echo \$!`
+
+                # Get the container IP Address
+                CONTAINER_IP=`docker inspect ${MICROSERVICE_NAME} | jq -r '.[0].NetworkSettings.IPAddress'`
+
+                # Let the application start
+                bash scripts/health_check.sh "http://\${CONTAINER_IP}:${MANAGEMENT_PORT}"
+
+                # Run tests
+                bash scripts/api_tests.sh \${CONTAINER_IP} ${MICROSERVICE_PORT}
+
+                # Kill process
+                kill \${PID}
+
+                # Kill Container
+                docker kill ${MICROSERVICE_NAME} || true
+                docker rm ${MICROSERVICE_NAME} || true
+                """
+            }
+            stage('Docker - Push Image to Registry') {
+                withCredentials([usernamePassword(credentialsId: registryCredsID,
+                                               usernameVariable: 'USERNAME',
+                                               passwordVariable: 'PASSWORD')]) {
+                    sh """
+                    #!/bin/bash
+
+                    # Get image
+                    if [ "${REGISTRY}" == "docker.io" ]; then
+                        IMAGE=${IMAGE_NAME}:${env.BUILD_NUMBER}
+                    else
+                        IMAGE=${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${env.BUILD_NUMBER}
+                    fi
+
+                    docker login -u ${USERNAME} -p ${PASSWORD} ${REGISTRY}
+
+                    docker push \${IMAGE}
+                    """
+                }
+            }
+        }
+    }
+}
